f95d65cdd55bf644903c517b1b1615f2afcd9909
rznalkh <alkhateeb.rzn@gmail.com>
2025-03-09 12:51:24 +0300

    Part1 tasks

--- src/functions.ts
+++ src/functions.ts
@@ -6,6 +6,7 @@
  */
 function printName(name: string): void {
   // write your code here
+  console.log(name);
 }
 
 /**
@@ -17,8 +18,10 @@ function printName(name: string): void {
  */
 function printAge(birthYear: number): number {
   // write your code here
-
-  return -1; // replace -1 to what you see is fit
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+  console.log(age);
+  return age; // replace -1 to what you see is fit
 }
 
 // example:
@@ -42,8 +45,16 @@ type LanguageType = "en" | "es" | "fr" | "tr";
 
 function printHello(name: string, language: LanguageType): string {
   // write your code here
-
-  return ""; // replace the empty string with what you see is fit
+if (language == "en" ){
+  return `Hello ${name}`
+}
+else if(language == "es"){
+  return `Hola ${name}`
+}
+else if(language == "fr"){
+  return `Bonjour ${name}`
+}
+  return `Merhaba ${name}`; // replace the empty string with what you see is fit
 }
 
 // example:
@@ -60,8 +71,13 @@ printHello("Aziz", "fr"); // => "Bonjour Aziz"
  */
 function printMax(x: number, y: number): number {
   // write your code here
+  if (x >= y){
+    console.log(x)
+    return x
 
-  return -1; // replace -1 to what you see is fit
+  }
+  console.log(y)
+  return y; // replace -1 to what you see is fit
 }
 
 // example:
